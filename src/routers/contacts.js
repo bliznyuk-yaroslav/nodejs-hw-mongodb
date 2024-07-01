87ff16815a1664e1e90b8ab4c2290b8058b58111
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-07-01 23:30:00 +0300

    1/2 homeWork Validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,6 +7,11 @@ import {
   getContactsByIdController,
   updateContactController,
 } from '../controllers/contacts.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+import { validateBody } from '../middlewares/validateBody.js';
 import isValid from '../middlewares/isValid.js';
 
 const contactsRouter = Router();
@@ -15,9 +20,18 @@ contactsRouter.get('/', ctrlWrapper(getAllContactsConroller));
 
 contactsRouter.get('/:id', isValid, ctrlWrapper(getContactsByIdController));
 
-contactsRouter.post('/', ctrlWrapper(createContactController));
+contactsRouter.post(
+  '/',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 
-contactsRouter.patch('/:id', isValid, ctrlWrapper(updateContactController));
+contactsRouter.patch(
+  '/:id',
+  isValid,
+  validateBody(updateContactSchema),
+  ctrlWrapper(updateContactController),
+);
 
 contactsRouter.delete('/:id', isValid, ctrlWrapper(deleteContactController));
 
