973d7f2ce118824000a58bfb68b13afd15bac3ef
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-07-11 09:35:54 +0300

    1/2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import pino from 'pino-http';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import contactsRouter from './routers/contacts.js';
+import router from './routers/index.js';
 import { env } from './utils/env.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import errorHandler from './middlewares/errorHandler.js';
@@ -19,9 +20,9 @@ export const setupServer = () => {
   );
   app.use(cors());
   app.use(express.json());
-  app.use('/contacts', contactsRouter);
-  app.use("*", notFoundHandler);
+  app.use(router);
+  app.use('*', notFoundHandler);
   app.use(errorHandler);
-
+  app.use(cookieParser());
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 };
